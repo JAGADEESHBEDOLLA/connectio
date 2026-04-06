540174ee1ca04608f6a436488b76c1e3c5117277
Sai Sumiran <164289250+Sumiran2105@users.noreply.github.com>
2026-04-06 16:41:13 +0530

    File setup done

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,9 @@
+import { Button } from "@/components/ui/button";
+
 export default function App() {
   return (
-    <div className="h-screen w-screen flex items-center justify-center bg-black text-white">
-      Tailwind v3 is working 🚀
+    <div className="h-screen w-screen flex items-center justify-center">
+      <Button>It works 🚀</Button>
     </div>
   );
 }
